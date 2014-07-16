6030aef70d985549774ed4f2344525ace8ddf0cb
Tony Rizko <tony@rizkocircle.com>
2014-07-16 03:17:13 -0700

    Adds handlebars templating engine and basic templates

--- youtube-music-stripper.js
+++ youtube-music-stripper.js
@@ -1,21 +1,27 @@
 var express = require('express');
+var handlebars = require('express-handlebars')
+  .create({ defaultLayout: 'main' });
 
 var app = express();
+
+//setup handlebars templating engine
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
-  res.send('Youtube music stripper');
+  res.render('home');
 });
 
 app.use(function(req, res){
   res.status(404);
-  res.send('404 - page not found');
+  res.render('404');
 });
 
 app.use(function(err, req, res, next){
   res.status(500);
-  res.send('500 - server error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), function(){
